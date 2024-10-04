323a37b10a6f9abec75ab3e149ee46d239a8f4f9
LuisChavarria0 <126618398+LuisChavarria0@users.noreply.github.com>
2024-10-04 10:12:19 -0600

    Cambio

--- wwwroot/assets/js/logoutLayout.js
+++ wwwroot/assets/js/logoutLayout.js
@@ -1,16 +1,6 @@
 ﻿function logoutSession() {
-    event.preventDefault();
-    $.ajax({
-        type: 'GET',
-        url: "/Acceso/Logout",
-        dataType: 'json',
-        success: function (result) {
-            const newUrl = window.location.origin + window.location.pathname;
-            window.history.pushState({ path: newUrl }, '', newUrl);
-        },
-        error: function (xhr, status, error) {
-            // Manejar errores si es necesario
-            console.error(error);
-        }
-    });
+    sessionStorage.clear();
+    localStorage.clear();
+    const newUrl = window.location.origin;
+    window.history.pushState({ path: newUrl }, '', newUrl);
 }
